049be5687c2af2ecac7a1d95a5ae9cd3310fac2a
Vitor <111993162+ViiTii@users.noreply.github.com>
2023-08-15 14:05:29 -0300

    GAME TESTE

---
+++ Suporte Avançado de Vida/story_content/user.js
@@ -0,0 +1,264 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "62Sw3t37aYp":
+        Script1();
+        break;
+      case "6ANAdkOf2XO":
+        Script2();
+        break;
+      case "6YJZmyCHNiG":
+        Script3();
+        break;
+      case "6N0pdlkEIu7":
+        Script4();
+        break;
+      case "5b48q3Q3AZ5":
+        Script5();
+        break;
+      case "5rww1qERGXE":
+        Script6();
+        break;
+      case "5tDgAtE7nu0":
+        Script7();
+        break;
+      case "6hfJKSSGez9":
+        Script8();
+        break;
+      case "5VqmyCX4uJZ":
+        Script9();
+        break;
+      case "6ZrxQSKi12C":
+        Script10();
+        break;
+      case "66eUzDmPF1d":
+        Script11();
+        break;
+      case "6Ni53j9lQ2d":
+        Script12();
+        break;
+      case "5gw2V4lve0w":
+        Script13();
+        break;
+      case "6pPVWjzP0zv":
+        Script14();
+        break;
+      case "6NGYaSzxz6e":
+        Script15();
+        break;
+      case "6PqSczSlYKz":
+        Script16();
+        break;
+      case "68PxNyLw8Pt":
+        Script17();
+        break;
+      case "6qfRas7YDmn":
+        Script18();
+        break;
+      case "5hHWi4k64qe":
+        Script19();
+        break;
+      case "6ThdgFJk2tw":
+        Script20();
+        break;
+      case "6q2rmIoQTe0":
+        Script21();
+        break;
+      case "6WPqno0k94a":
+        Script22();
+        break;
+      case "5jgMXLNn2ix":
+        Script23();
+        break;
+      case "5lWOh4aFXgc":
+        Script24();
+        break;
+      case "6WWrgZWSCVJ":
+        Script25();
+        break;
+      case "6InrLJ5ZmSO":
+        Script26();
+        break;
+      case "6HWfTr7Mh3M":
+        Script27();
+        break;
+      case "5yKlDNIMhJh":
+        Script28();
+        break;
+      case "5t5u5cDKwSE":
+        Script29();
+        break;
+      case "66K1KdlSmkm":
+        Script30();
+        break;
+      case "5mbV2jYFsT0":
+        Script31();
+        break;
+      case "5swCN63zZPs":
+        Script32();
+        break;
+  }
+}
+
+function Script1()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",1);
+}
+
+function Script2()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",2);
+}
+
+function Script3()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",3);
+}
+
+function Script4()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",4);
+}
+
+function Script5()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",5);
+}
+
+function Script6()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",6);
+}
+
+function Script7()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",7);
+}
+
+function Script8()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",8);
+}
+
+function Script9()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",9);
+}
+
+function Script10()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",10);
+}
+
+function Script11()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",11);
+}
+
+function Script12()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",12);
+}
+
+function Script13()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",13);
+}
+
+function Script14()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",14);
+}
+
+function Script15()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",15);
+}
+
+function Script16()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",16);
+}
+
+function Script17()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",17);
+}
+
+function Script18()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",18);
+}
+
+function Script19()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",19);
+}
+
+function Script20()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",20);
+}
+
+function Script21()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",21);
+}
+
+function Script22()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",22);
+}
+
+function Script23()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",23);
+}
+
+function Script24()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",24);
+}
+
+function Script25()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",25);
+}
+
+function Script26()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",26);
+}
+
+function Script27()
+{
+  SCORM_CallLMSFinish
+SCORM_CallLMSFinish("");
+}
+
+function Script28()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",28);
+}
+
+function Script29()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",29);
+}
+
+function Script30()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",30);
+}
+
+function Script31()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",31);
+}
+
+function Script32()
+{
+  SCORM_CallLMSSetValue("cmi.core.lesson_location",32);
+}
+
